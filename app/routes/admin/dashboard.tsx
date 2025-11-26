a1f685a8926ec88db3f413b1457d93a32052fc38
Abhinav Gaur <abhinavajgaur@gmail.com>
2025-11-26 19:48:24 +0530

    updated work

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,7 +1,92 @@
+import { StatsCard, TripCard } from "components";
+import Header from "components/Header";
+
+const user = {
+  name: 'Adrian'
+};
+
+const dashboardStats = {
+  totalUsers: 12450,
+  usersJoined: { currentMonth: 128, lastMonth: 176 },
+  totalTrips: 3210,
+  tripsCreated: {currentMonth: 150, lastMonth: 250},
+  userRole: { total: 62, currentMonth: 25, lastMonth: 15},
+};
+
+const allTrips = [{
+      id: 1,
+      name: "Tropical Rewind",
+      imageUrls: ["/assets/images/sample1.jpg"],
+      itinerary: [{ location: "Thailand" }],
+      tags: ["Adventure", "Culture"],
+      travelStyle: "Solo",
+      estimatedPrice: "$1,000",
+    },
+    {
+      id: 2,
+      name: "French Reverie",
+      imageUrls: ["/assets/images/sample2.jpg"],
+      itinerary: [{ location: "Paris" }],
+      tags: ["Relaxation", "Culinary"],
+      travelStyle: "Family",
+      estimatedPrice: "$2,000",
+    },
+    {
+      id: 3,
+      name: "Zen Break",
+      imageUrls: ["/assets/images/sample3.jpg"],
+      itinerary: [{ location: "Japan" }],
+      tags: ["Shopping", "Luxury"],
+      travelStyle: "Couple",
+      estimatedPrice: "$3,000",
+    },
+    {
+      id: 4,
+      name: "Adventure in Westeros",
+      imageUrls: ["/assets/images/sample4.jpg"],
+      itinerary: [{ location: "Croatia" }],
+      tags: ["Historical", "Culture"],
+      travelStyle: "Friends",
+      estimatedPrice: "$4,000",
+    },
+    ];
+
+
 export default function Dashboard() {
+
+  const {totalUsers, usersJoined, totalTrips, tripsCreated, userRole} = dashboardStats;
+
   return (
-    <div>
-      Dashboard page
-    </div>
+    <main className="dashboard wrapper">
+      <Header
+          title={`Welcome ${user?.name ?? 'Guest'}👋`}
+          description="Track activity, trends and popular destination"
+      />
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full
+        ">
+          <StatsCard
+            headerTitle = "Total Users"
+            total = {totalUsers}
+            currentMonthCount = {usersJoined.currentMonth}
+            lastMonthCount = {usersJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle = "Total Trips"
+            total = {totalTrips}
+            currentMonthCount = {tripsCreated.currentMonth}
+            lastMonthCount = {tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle = "Active Users"
+            total = {userRole.total}
+            currentMonthCount = {userRole.currentMonth}
+            lastMonthCount = {userRole.lastMonth}
+          />
+        </div>
+      </section>
+      
+      <TripCard/>
+    </main>
   );
 }
